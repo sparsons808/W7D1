520567ac803364c9bef3a07b126bab589d82ed8f
Sydney <sydneyparsonsyb@gmail.com>
2021-07-26 15:35:04 -0400

    views_session

--- cats/app/controllers/users_controller.rb
+++ cats/app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
         render :new
     end
 
-    
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_url(@user)
+            #will need to login
+        else
+            render :new
+        end
+        
+    end
+
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:user_name, :password)
+    end
 end
--- cats/config/routes.rb
+++ cats/config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  resource :session, only: [:new, :create, :destroy]
+
   root to: redirect('/cats')
 end
